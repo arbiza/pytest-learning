a4e8ca1dfb90302bff7a81b6c9713a851456e921
Lucas Arbiza <lucas.arbiza@gmail.com>
2022-11-23 11:44:33 +0100

    Used fixture to input data to the tests

--- test_user_class.py
+++ test_user_class.py
@@ -1,15 +1,26 @@
+# Run this test with the capture=no argmument:
+#
+#    pytest --capture=no
+
 import pytest
 from main import User
 from main import SimpleError
 
 
+@pytest.fixture
+def users():
+
+    users_list = list()
+    users_list.append(User('John', 1))
+    users_list.append(User('Mary', 2, False))
+
+    print(">> Built users database")
+
+    return users_list
+
+
 @pytest.mark.happy
-def test_user_create():
-
-    users = []
-
-    users.append(User('John', 1, True))
-    users.append(User('Mary', 2, False))
+def test_user_create(users):
 
     assert users[0].name == 'John'
     assert users[0].id == 1
@@ -21,43 +32,38 @@ def test_user_create():
 
 
 @pytest.mark.happy
-def test_user_create_minimun_args():
-    user = User('John', 1)
-
-    assert user.name == 'John'
-    assert user.id == 1
-    assert user.status == 'Active'
+def test_user_create_minimun_args(users):
+    assert users[0].name == 'John'
+    assert users[0].id == 1
+    assert users[0].status == 'Active'
 
 
 @pytest.mark.sad
-def test_user_create_missing_args():
+def test_user_create_missing_args(users):
     with pytest.raises(TypeError) as exp:
         user = User()
     assert "__init__() missing 2 required positional arguments: 'name' and 'id'" in str(exp.value)
 
 
 @pytest.mark.sad
-def test_user_change_name():
-    user = User('John', 1)
+def test_user_change_name(users):
     with pytest.raises(SimpleError) as exp:
-        user.name = 'Marcus'
+        users[0].name = 'Marcus'
         pass
     assert str(exp.value) == "The 'name' attribute can't be set/changed directly"
 
 
 @pytest.mark.sad
-def test_user_change_id():
-    user = User('John', 1)
+def test_user_change_id(users):
     with pytest.raises(SimpleError) as exp:
-        user.id = 10
+        users[0].id = 10
         pass
     assert str(exp.value) == "The 'id' attribute can't be set/changed directly"
 
 
 @pytest.mark.sad
-def test_user_change_status():
-    user = User('John', 1)
+def test_user_change_status(users):
     with pytest.raises(SimpleError) as exp:
-        user.status = 'Inactive'
+        users[0].status = 'Inactive'
         pass
     assert str(exp.value) == "The 'status' attribute can't be set/changed directly"
