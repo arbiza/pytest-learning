fd4da5840f244888136b04f5c37b33e68ddf8a8f
Lucas Arbiza <lucas.arbiza@gmail.com>
2022-11-23 13:57:03 +0100

    Runs with scope and yield

--- test_user_class.py
+++ test_user_class.py
@@ -7,16 +7,20 @@ from main import User
 from main import SimpleError
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def users():
 
     users_list = list()
     users_list.append(User('John', 1))
     users_list.append(User('Mary', 2, False))
 
-    print(">> Built users database")
+    print("\n>> Built users database -- fixture function")
 
-    return users_list
+    yield users_list
+
+    # Because of the 'yield' above instead of 'return', the following will be
+    # executed after the caller function ends.
+    print("\n>> After tests, it gets back to the fixture function.")
 
 
 @pytest.mark.happy
@@ -30,6 +34,8 @@ def test_user_create(users):
     assert users[1].id == 2
     assert users[1].status == 'Inactive'
 
+    print(users[-1])
+
 
 @pytest.mark.happy
 def test_user_create_minimun_args(users):
@@ -37,12 +43,25 @@ def test_user_create_minimun_args(users):
     assert users[0].id == 1
     assert users[0].status == 'Active'
 
+    print(users[-1])
+
+
+@pytest.mark.happy
+def test_user_create_new_user(users):
+
+    users.append(User('New guy', 3))
+    assert len(users) == 3
+
+    print(users[-1])
+
 
 @pytest.mark.sad
 def test_user_create_missing_args(users):
     with pytest.raises(TypeError) as exp:
         user = User()
     assert "__init__() missing 2 required positional arguments: 'name' and 'id'" in str(exp.value)
+
+    print(users[-1])
 
 
 @pytest.mark.sad
@@ -52,6 +71,8 @@ def test_user_change_name(users):
         pass
     assert str(exp.value) == "The 'name' attribute can't be set/changed directly"
 
+    print(users[-1])
+
 
 @pytest.mark.sad
 def test_user_change_id(users):
@@ -60,6 +81,8 @@ def test_user_change_id(users):
         pass
     assert str(exp.value) == "The 'id' attribute can't be set/changed directly"
 
+    print(users[-1])
+
 
 @pytest.mark.sad
 def test_user_change_status(users):
@@ -67,3 +90,5 @@ def test_user_change_status(users):
         users[0].status = 'Inactive'
         pass
     assert str(exp.value) == "The 'status' attribute can't be set/changed directly"
+
+    print(users[-1])
